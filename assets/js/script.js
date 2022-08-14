41f7582306c5f8ca7d0dd99b151c757cf83913b2
Noah Adam <75527685+na110@users.noreply.github.com>
2022-08-14 13:15:00 -0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+// N A V B A R    T O G G L E
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+const elemArr = [navCloseBtn, overlay, navOpenBtn];
+
+for (let i = 0; i < elemArr.length; i++) {
+  elemArr[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
+
+// T O G G L E     N A V B A R    &    O V E R L A Y    W H E N     C L I C K    A N Y    N A V B A R    L I N K
+const navbarLinks = document.querySelectorAll("[data-navbar-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
+
+
+// H E A D E R    &    G O    T O P    B T N    A C T I V E    W H E N    W I N D O W    S C R O L L    D O W N    T O    400PX
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 400) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+});
